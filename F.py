5ee61925c5f71fb02cdbf7d80ada8f50b9717cf1
FADIL AHMED <benabdi@10-248-163-86.wireless.oregonstate.edu>
2020-08-10 12:44:06 -0700

    I did it

---
+++ F.py
@@ -0,0 +1,10 @@
+import json
+import pymongo
+import os
+
+
+#fp = open("/Users/benabdi/Desktop/mongodb/__pycache__/js.json", "r")
+# This file for jason file
+fp = open("/Users/benabdi/Desktop/json/index.json")
+mydata = fp.read()
+fp.close()
